b511e1f511074290a2a03cb18cfc7f1b6ddd6a85
Moroka <mmopanemoroka@gmail.com>
2023-06-14 21:01:55 +0100

    handling music function and music icons

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { GlobalStyles } from "./styles/Global.styled";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./styles/theme";
 import { ThemeContext } from "./contexts/ThemeContext";
- 
+import MusicPlayer from "./components/MusicPlayer/MusicPlayer";
+
 function App() {
   const { theme } = useContext(ThemeContext);
   const mode = theme === "light" ? lightTheme : darkTheme;
@@ -12,6 +13,7 @@ function App() {
     <ThemeProvider theme={mode}>
       <GlobalStyles />
       <Router />
+      <MusicPlayer />
     </ThemeProvider>
   );
 }
